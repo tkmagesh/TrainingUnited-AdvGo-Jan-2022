aac9b1667d82c0f51edd63cd53fc5e334eb44609
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-26 18:14:09 +0530

    assignment-1 - partial solution

--- 02-concurrency/assignment-1.go
+++ 02-concurrency/assignment-1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -23,21 +24,24 @@ Important :
 
 */
 
+var wg = &sync.WaitGroup{}
+
 func main() {
-	printHello()
-	printWorld()
+	wg.Add(2)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	go print("Hello", ch1, ch2)
+	go print("World", ch2, ch1)
+	ch1 <- true
+	wg.Wait()
 }
 
-func printHello() {
+func print(s string, in, out chan bool) {
 	for i := 0; i < 5; i++ {
+		<-in
 		time.Sleep(500 * time.Millisecond)
-		fmt.Println("Hello")
+		fmt.Println(s)
+		out <- true
 	}
-}
-
-func printWorld() {
-	for i := 0; i < 5; i++ {
-		time.Sleep(500 * time.Millisecond)
-		fmt.Println("World")
-	}
+	wg.Done()
 }
