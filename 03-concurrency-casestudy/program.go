5b60527263e2819b6382c8a6336b3a189050dd7d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-26 20:57:21 +0530

    concurrency-casestudy - solution

--- 03-concurrency-casestudy/program.go
+++ 03-concurrency-casestudy/program.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 )
 
 /*
@@ -15,21 +16,85 @@ Problem statement:
 	Write the sum of even numbers & sum of odd number into another file "result.txt"
 */
 func main() {
-	var evenSum, oddSum int
-	file, err := os.Open("data2.dat")
+	dataCh := make(chan int)
+	fileWg := &sync.WaitGroup{}
+	fileWg.Add(2)
+	go Source("data1.dat", dataCh, fileWg)
+	go Source("data2.dat", dataCh, fileWg)
+
+	processWg := &sync.WaitGroup{}
+	evenSumCh := make(chan int)
+	oddSumCh := make(chan int)
+
+	processWg.Add(3)
+	evenCh, oddCh := Splitter(dataCh)
+	go Sum(evenCh, evenSumCh, processWg)
+	go Sum(oddCh, oddSumCh, processWg)
+	go Merger(evenSumCh, oddSumCh, "result.txt", processWg)
+	fileWg.Wait()
+	close(dataCh)
+
+	processWg.Wait()
+	fmt.Println("Done")
+}
+
+func Source(filename string, dataCh chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		txt := scanner.Text()
 		if no, err := strconv.Atoi(txt); err == nil {
-			if no%2 == 0 {
-				evenSum += no
-			} else {
-				oddSum += no
-			}
+			dataCh <- no
 		}
 	}
-	fmt.Println(evenSum, oddSum)
+}
+
+func Splitter(dataCh chan int) (even, odd chan int) {
+	even = make(chan int)
+	odd = make(chan int)
+	go func() {
+		for no := range dataCh {
+			if no%2 == 0 {
+				even <- no
+			} else {
+				odd <- no
+			}
+		}
+		close(even)
+		close(odd)
+	}()
+	return
+}
+
+func Sum(noCh chan int, resultCh chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	sum := 0
+	for no := range noCh {
+		sum += no
+	}
+	resultCh <- sum
+}
+
+func Merger(evenSum, oddSumCh chan int, resultFile string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	file, err := os.Create(resultFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+	for i := 0; i < 2; i++ {
+		select {
+		case evenSumVal := <-evenSum:
+			writer.WriteString("Even total = " + strconv.Itoa(evenSumVal) + "\n")
+		case oddSumVal := <-oddSumCh:
+			writer.WriteString("Odd total = " + strconv.Itoa(oddSumVal) + "\n")
+		}
+	}
 }
