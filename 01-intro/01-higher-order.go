61a65db880d48c7b35ccd8b5a418ed9c3adfe130
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-26 14:20:50 +0530

    higher order functions - passing functions as arguments

---
+++ 01-intro/01-higher-order.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fn func()
+
+	fn = func() {
+		fmt.Println("fn is invoked")
+	}
+	//fn()
+	exec(fn)
+
+	/* fmt.Println(add(100, 200))
+	fmt.Println(subtract(100, 200)) */
+	fmt.Println(logOperation(add, 100, 200))
+	fmt.Println(logOperation(subtract, 100, 200))
+}
+
+/* func fn() {
+	fmt.Println("fn is invoked")
+} */
+
+func exec(f func()) {
+	f()
+}
+
+func logOperation(operation func(int, int) int, x, y int) int {
+	fmt.Println("before invocation")
+	result := operation(x, y)
+	fmt.Println("after invocation")
+	return result
+}
+
+func add(x, y int) int {
+	result := x + y
+	return result
+}
+
+func subtract(x, y int) int {
+	result := x - y
+	return result
+}
