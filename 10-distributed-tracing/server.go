7292e41c582ba79e29f7c77fb4e61b79c05fdac1
Magesh Kuppan <tkmagesh77@gmail.com>
2022-01-28 18:52:48 +0530

    creating traces & spans

--- 10-distributed-tracing/server.go
+++ 10-distributed-tracing/server.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 	"strings"
 
+	"distributed-tracing/lib/tracing"
 	"distributed-tracing/people"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var repo *people.Repository
+var tracer trace.Tracer
 
 func main() {
+	tracer = tracing.Init("hello-server")
 	repo = people.NewRepository()
 	defer repo.Close()
 
@@ -21,12 +27,16 @@ func main() {
 }
 
 func handleSayHello(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "sayHello")
+	defer span.End()
+
 	name := strings.TrimPrefix(r.URL.Path, "/sayHello/")
 	greeting, err := SayHello(name)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	span.AddEvent("Sending Response :", trace.WithAttributes(attribute.String("greeting", greeting)))
 	w.Write([]byte(greeting))
 }
 
